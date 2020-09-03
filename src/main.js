9d36f30698cb3a8d84adb073c04804ff5450b8d6
zhangguangpu <1136062041@qq.com>
2020-09-03 17:40:09 +0800

    Vuex调整

--- src/main.js
+++ src/main.js
@@ -2,18 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.use(Vuex)
 
 Vue.config.productionTip = false
-
-const store = new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-})
 
 new Vue({
   router,
